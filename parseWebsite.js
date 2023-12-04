9b6416638c366298590152ae504634a30bb4117e
HudsonM64 <h8009916@gmail.com>
2023-12-04 17:57:01 -0600

    updated parking data

--- parseWebsite.js
+++ parseWebsite.js
@@ -1,72 +1,75 @@
-// npm install puppeteer fs
 const puppeteer = require('puppeteer');
-const fs = require('fs');
+const fs = require('fs').promises; // Import promises-based fs module for writeFile
 
-async function getParkingData() {
-  try {
-    // Launch a headless browser with the new headless mode
-    const browser = await puppeteer.launch({ headless: 'new' });
-    const page = await browser.newPage();
+async function getParkingData(url, outputFilename) {
+  const browser = await puppeteer.launch({
+    headless: false, // Set to true if you don't need a browser UI
+    defaultViewport: null,
+  });
 
-    // URL of the website with parking information
-    const url = 'https://www.lsu.edu/parking/availability.php';
+  const page = await browser.newPage();
 
-    // Navigate to the website
-    await page.goto(url, { waitUntil: 'domcontentloaded' });
+  await page.goto(url, {
+    waitUntil: "domcontentloaded",
+  });
 
-    // Wait for a specific selector to appear (adjust as needed)
-    await page.waitForSelector('.accordion-section-2', { timeout: 5000 });
+  // Scrape the data
+  const data = await page.evaluate(() => {
+    let allData = [];
+    const accordionSections = document.querySelectorAll(".accordion-section-2");
 
-    // Extract parking information using Puppeteer
-    const parkingData = await page.evaluate(() => {
-      const data = [];
+    accordionSections.forEach((section) => {
+      // Find the h2 element that precedes the accordion-section-2
+      let headerElement = section.previousElementSibling;
+      while (headerElement && headerElement.tagName.toLowerCase() !== "h2") {
+        headerElement = headerElement.previousElementSibling;
+      }
+      const permitType = headerElement
+        ? headerElement.textContent.trim()
+        : "Unknown Permit Type";
 
-      // Iterate over each accordion section
-      document.querySelectorAll('.accordion-section-2').forEach((accordionSection) => {
-        // Extract parking lot data from the table
-        const tableRows = accordionSection.querySelectorAll('table.ou_6col tbody tr');
-        tableRows.forEach((row, rowIndex) => {
-          try {
-            const lotName = row.querySelector('td:nth-child(1)').innerText.trim();
-            const lotNumber = row.querySelector('td:nth-child(2)').innerText.trim();
-            const totalSpaces = row.querySelector('td:nth-child(3)').innerText.trim();
-            const times = Array.from(row.querySelectorAll('td:nth-child(n+4)')).map((cell) =>
-              cell.innerText.trim()
-            );
+      // Now find all the .card elements within the section to get the day and lot details
+      const cards = section.querySelectorAll(".card");
+      cards.forEach((card) => {
+        const dayOfWeek = card
+          .querySelector(".card-header h2")
+          .textContent.trim();
+        const rows = card.querySelectorAll(".table tbody tr:not(:first-child)");
 
-            // Create a parking lot object
-            const parkingLot = {
-              lotName,
-              lotNumber,
-              totalSpaces,
-              times,
-            };
-
-            // Add the parking lot object to the array
-            data.push(parkingLot);
-          } catch (error) {
-            console.error(`Error processing row ${rowIndex + 1}:`, error.message);
-          }
+        rows.forEach((row) => {
+          const cells = row.querySelectorAll("td");
+          const lotData = {
+            permitType,
+            dayOfWeek,
+            lotName: cells[0].textContent.trim(),
+            lotNumber: cells[1].textContent.trim(),
+            totalSpaces: cells[2].textContent.trim(),
+            occupancy: {
+              "7am": cells[3].textContent.trim(),
+              "11am": cells[4].textContent.trim(),
+              "2pm": cells[5].textContent.trim(),
+              "4pm": cells[6].textContent.trim(),
+            },
+          };
+          allData.push(lotData);
         });
       });
-
-      return data;
     });
 
-    // Close the browser
-    await browser.close();
+    return allData;
+  });
 
-    // Write the parking data to a JSON file
-    const jsonData = JSON.stringify(parkingData, null, 2); // 2 spaces for indentation
-    fs.writeFileSync('parkingData.json', jsonData);
-    console.log('JSON data has been written to parkingData.json');
+  // Use promises-based fs.writeFile
+  await fs.writeFile(outputFilename, JSON.stringify(data, null, 2));
+  console.log(`Data saved to ${outputFilename}`);
 
-    // Output the parking data as a JSON object
-    console.log(JSON.stringify(parkingData, null, 2));
-  } catch (error) {
-    console.error('Error:', error.message);
-  }
+  await browser.close();
 }
 
-// Call the function to get parking data
-getParkingData();
+// Example usage
+getParkingData(
+  "https://www.lsu.edu/parking/availability.php",
+  "parkingData.json"
+).catch((error) => {
+  console.error("Error occurred:", error);
+});
